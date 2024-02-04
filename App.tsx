97351612c67fe4fbf3be646a73f119c652336713
Le Phan tuan Anh <anhroyal110@gmail.com>
2024-02-05 00:50:11 +0700

    feature change app

--- App.tsx
+++ App.tsx
@@ -5,11 +5,14 @@ import {AuthProvider} from './src/utils';
 import Navigation from './src/navigation';
 import Toast from 'react-native-toast-message';
 import ToastCustom from './src/components/Toast';
+import {useKeepAwake} from '@sayem314/react-native-keep-awake';
 
 const App = () => {
   useEffect(() => {
     setTimeout(() => SplashScreen.hide(), 1000);
   }, []);
+
+  useKeepAwake();
 
   return (
     <>
